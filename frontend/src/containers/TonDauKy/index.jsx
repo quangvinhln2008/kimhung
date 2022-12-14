d56cb46a815687f719055715c37144ea132544db
Nguyen Vinh <vinhnq.ufm@gmail.com>
2022-12-14 08:35:10 +0700

    cap nhat form import ton dau ky

--- frontend/src/containers/TonDauKy/index.jsx
+++ frontend/src/containers/TonDauKy/index.jsx
@@ -2,9 +2,10 @@ import React, {useState, useEffect} from "react";
 import axios from 'axios'
 import moment from 'moment'
 import { toast } from 'react-toastify'
-import { Divider, Typography, Button, Select, Modal, Space, DatePicker, InputNumber, Input, Table, Form, Tag, Popconfirm , Alert, Spin} from 'antd';
-import { SearchOutlined, PlusCircleOutlined } from '@ant-design/icons';
+import { Divider, Typography, Tabs, Button, Select, Modal, Space, DatePicker, InputNumber, Input, Table, Form, Tag, Popconfirm , Alert, Spin} from 'antd';
+import { ImportOutlined, PlusCircleOutlined } from '@ant-design/icons';
 import {VStack, HStack} from  '@chakra-ui/react';
+import { read, utils, writeFile } from 'xlsx';
 
 const { Title } = Typography;
 const { Option } = Select;
@@ -18,9 +19,12 @@ const TonDauKy = () =>{
   const [dataVatTu, setDataVatTu] = useState()
   const [dataKho, setDataKho] = useState()
   const [dataKichThuoc, setDataKichThuoc] = useState()
+  const [dataImportTonDauKy, setDataImportTonDauKy] = useState()
   const [loading, setLoading] = useState(true);
   const [refresh, setRefresh] = useState(false)
   const [openModalContact, setOpenModalContact] = useState(false)
+  const [openModalImportTonDauKy, setOpenModalImportTonDauKyu] = useState(false)
+  const [dataCheckNhomVatTu, setDataCheckNhomVatTu] = useState()
   const [optionsVatTu, setOptionVatTu] = useState()
   const [optionsKichThuoc, setOptionKichThuoc] = useState()
   const [optionsKho, setOptionKho] = useState()
@@ -31,6 +35,28 @@ const TonDauKy = () =>{
     setOpenModalContact(!openModalContact)
   }
 
+  function toogleModalFormImportTonDauKy(){
+    setOpenModalImportTonDauKyu(!openModalImportTonDauKy)
+  }
+
+  const handleImportTonDauKy = ($event) => {
+    const files = $event.target.files;
+    if (files.length) {
+        const file = files[0];
+        const reader = new FileReader();
+        reader.onload = (event) => {
+            const wb = read(event.target.result);
+            const sheets = wb.SheetNames;
+
+            if (sheets.length) {
+                const rows = utils.sheet_to_json(wb.Sheets[sheets[0]]);
+                setDataImportTonDauKy(rows)
+                setDataCheckNhomVatTu(null)
+            }
+        }
+        reader.readAsArrayBuffer(file);
+    }
+}
   useEffect(()=>{
     loadTonDauKy()
   },[refresh])
@@ -187,6 +213,27 @@ const TonDauKy = () =>{
       })
   };
 
+  async function importTonDauKy(){
+    return await axios
+      .post('https://testkhaothi.ufm.edu.vn:3002/import/tondauky', {
+        DataImportTonDauKy: dataImportTonDauKy
+      })
+      .then((res) => {
+        const result = {
+          status: res.status,
+          data: res.data.result.recordsets,
+        }
+        setDataCheckNhomVatTu(result?.data[1])
+        result?.data[0][0].status === 200 ? toast.success(result?.data[0][0].message): toast.error(result?.data[0][0].message)
+        return(result)
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error.response)
+        toast.error(error?.response)
+      })
+  };
+
   const onChange = (pagination, filters, sorter, extra) => {
     // const filterNhom = dataVatTuFilter.filters(item => item.TenNhomVatTu === filters?.TenNhomVatTu[0])
     // setFilterVt(filterNhom)
@@ -248,6 +295,31 @@ const TonDauKy = () =>{
     },
   ];
 
+  const columnsImportTonDauKy = [
+    {
+      title: 'Mã vật tư',
+      dataIndex: 'MaVatTu',
+      key: 'MaVatTu',
+    },
+    {
+      title: 'Tên vật tư',
+      dataIndex: 'TenVatTu',
+      key: 'TenVatTu',
+    },
+    {
+      title: 'Số lượng tồn',
+      dataIndex: 'SoLuongTon',
+      align:'right',
+      key: 'SoLuongTon',
+    },
+    {
+      title: 'Đơn giá tồn',
+      dataIndex: 'DonGiaTon',
+      align:'right',
+      key: 'DonGiaTon',
+    }
+  ];
+
   return(
     <>
       <Title level={3}>Tồn kho vật tư đầu kỳ</Title>
@@ -257,8 +329,8 @@ const TonDauKy = () =>{
           <Button  onClick={openCreateMode}  type="primary" icon={<PlusCircleOutlined />}>
               Thêm mới
           </Button>
-          <Button  onClick={toogleModalFormContact} icon={<SearchOutlined />}>
-              Tìm kiếm
+          <Button onClick={toogleModalFormImportTonDauKy} icon={<ImportOutlined />}>
+              Thêm mới vật tư bằng file Excel
           </Button>
         </Space>
         <Divider />
@@ -394,6 +466,61 @@ const TonDauKy = () =>{
             <Button key="save" type="primary"  htmlType="submit">Lưu</Button>
           </HStack>
         </Form>
+      </Modal>
+
+      {/* Modal thêm mới bằng file excel */}
+      <Modal 
+        open={openModalImportTonDauKy}
+        title={"Thêm mới tồn đầu kỳ bằng file Excel"}
+        onCancel={toogleModalFormImportTonDauKy}
+        footer={null}
+        width={1000}
+      >
+      <Form form={form} 
+          labelCol={{
+            span: 8,
+          }}
+          wrapperCol={{
+            span: 20,
+          }}
+          // onFinish={importVatTu}
+        >          
+          <Form.Item
+            name="upload"
+            label="Chọn file"
+          >
+            <input type="file" name="file" className="custom-file-input" id="inputGroupFile" required onChange={handleImportTonDauKy}
+                accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"/>
+            <label className="custom-file-label" htmlFor="inputGroupFile">Chọn file import</label>           
+            
+          </Form.Item>
+          <Tabs
+            defaultActiveKey="1"
+            onChange={onChange}
+            items={[
+              {
+                label: `Dữ liệu import`,
+                key: '1',
+                children: (<>
+                  <Table columns={columnsImportTonDauKy} dataSource={dataImportTonDauKy} />
+                </>),
+              },
+              {
+                label: `Lỗi`,
+                key: '2',
+                children: (<>
+                  <Table columns={columnsImportTonDauKy} dataSource={dataCheckNhomVatTu} />
+                </>),
+              }
+            ]}
+          />
+          
+        </Form>
+        
+        <HStack justifyContent="end">
+            <Button key="back" onClick={toogleModalFormImportTonDauKy}>Thoát</Button>
+            <Button key="save" type="primary"  onClick={importTonDauKy}>Import tồn đầu kỳ</Button>
+          </HStack>
       </Modal>
     </>
   )
